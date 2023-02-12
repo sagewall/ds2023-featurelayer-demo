703cd8cbe97b6a49c1054e3dd7d9747dd1fc26b4
Sage Wall <sagewall@users.noreply.github.com>
2023-02-12 10:35:02 -0700

    add simple renderer with visual variables

--- main.js
+++ main.js
@@ -7,38 +7,71 @@ import LayerList from "@arcgis/core/widgets/LayerList.js";
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import "@esri/calcite-components/dist/components/calcite-shell.js";
 import { setAssetPath } from "@esri/calcite-components/dist/components/index.js";
-import { cloudSymbol, sunSymbol } from "./lib.js";
+import { arrowSymbol } from "./lib.js";
 import "./style.css";
 
 setAssetPath("https://js.arcgis.com/calcite-components/1.0.7/assets");
 
 /**
- * arcade expressions
+ * constants
  */
 
-const skyConditionsExpression = `
-    IIf(Find( 'Clear', $feature.SKY_CONDTN ) >= 0, 'sunny', 'cloudy');
-  `;
+const referenceScale = 577790.554289;
+
+const temperatureStops = [
+  { value: 0, color: "#4575b4" },
+  { value: 32, color: "#91bfdb" },
+  { value: 55, color: "#e0f3f8" },
+  { value: 80, color: "#fee090" },
+  { value: 100, color: "#fc8d59" },
+  { value: 150, color: "#d73027" }
+];
 
 /**
  * renderer
  */
 
 const renderer = {
-  type: "unique-value",
-  uniqueValueInfos: [
+  type: "simple",
+  symbol: arrowSymbol,
+  visualVariables: [
     {
-      label: "Sunny",
-      symbol: sunSymbol,
-      value: "sunny"
+      type: "color",
+      field: "TEMP",
+      stops: temperatureStops
     },
     {
-      label: "Cloudy",
-      symbol: cloudSymbol,
-      value: "cloudy"
+      type: "rotation",
+      field: "WIND_DIRECT",
+      rotationType: "geographic"
+    },
+    {
+      type: "size",
+      field: "WIND_SPEED",
+      minDataValue: 5,
+      minSize: {
+        type: "size",
+        valueExpression: "$view.scale",
+        stops: [
+          { value: referenceScale * 32, size: 16 },
+          { value: referenceScale * 64, size: 12 },
+          { value: referenceScale * 128, size: 8 },
+          { value: referenceScale * 256, size: 4 }
+        ]
+      },
+      maxDataValue: 100,
+      maxSize: {
+        type: "size",
+        valueExpression: "$view.scale",
+        stops: [
+          { value: referenceScale * 32, size: 40 },
+          { value: referenceScale * 64, size: 30 },
+          { value: referenceScale * 128, size: 20 },
+          { value: referenceScale * 256, size: 10 }
+        ]
+      }
     }
-  ],
-  valueExpression: skyConditionsExpression
+  ]
 };
 
 /**
